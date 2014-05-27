ae2fc40ddbdba9e1600c00ec4563256a46e15247
Rodrigo Dumont <rlsdumont@gmail.com>
2014-05-27 19:53:30 -0300

    Cria estrutura básica da solução

---
+++ src/Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vtex.Toolbelt.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from VTEX!");
+        }
+    }
+}
