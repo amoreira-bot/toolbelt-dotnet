bac1d2fea7e5faa01ad73c58524bbb4e1bd47c28
Rodrigo Dumont <rlsdumont@gmail.com>
2014-07-08 15:29:27 -0300

    Create credential store
    The credential is stored in a file called `credentials.json`, side by side
    with the binaries.

---
+++ src/Core/Credential.cs
@@ -0,0 +1,18 @@
+﻿namespace Vtex.Toolbelt.Core
+{
+    public class Credential
+    {
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+        public Credential()
+        {
+        }
+
+        public Credential(string email, string token)
+        {
+            this.Email = email;
+            this.Token = token;
+        }
+    }
+}
---
+++ src/Core/CredentialStore.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Vtex.Toolbelt.Core
+{
+    public class CredentialStore
+    {
+        private static readonly string FilePath = Path.Combine(
+            Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+            "credentials.json");
+        private static readonly Encoding DefaultEncoding = Encoding.UTF8;
+
+        public Credential GetCurrent()
+        {
+            if (!File.Exists(FilePath))
+                return null;
+
+            var json = File.ReadAllText(FilePath, DefaultEncoding);
+            return JsonConvert.DeserializeObject<Credential>(json);
+        }
+
+        public void Save(Credential credential)
+        {
+            var json = JsonConvert.SerializeObject(credential, Formatting.Indented);
+            File.WriteAllText(FilePath, json, DefaultEncoding);
+        }
+    }
+}
