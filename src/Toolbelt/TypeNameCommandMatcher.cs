0c9fe5433cd3c7dd387079b9000c509a86a6e605
Rodrigo Dumont <rlsdumont@gmail.com>
2014-07-22 13:10:36 -0300

    Implement type name command matcher

---
+++ src/Toolbelt/TypeNameCommandMatcher.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vtex.Toolbelt
+{
+    public class TypeNameCommandMatcher : ICommandMatcher
+    {
+        private readonly IEnumerable<Type> _candidateTypes;
+
+        public TypeNameCommandMatcher(IEnumerable<Type> candidateTypes)
+        {
+            _candidateTypes = candidateTypes;
+        }
+
+        public bool TryGetMatchedType(string[] args, out Type commandType, out int usedArgCount)
+        {
+            var commandArguments = args.TakeWhile(arg => !arg.StartsWith("-")).Take(2).ToArray();
+            var commandName = commandArguments.Any()
+                ? string.Concat(commandArguments.Skip(1).FirstOrDefault(), commandArguments[0])
+                : "help";
+            var typeName = string.Concat(commandName, "command").ToLower();
+
+            commandType = _candidateTypes.FirstOrDefault(type => type.Name.ToLower() == typeName);
+            usedArgCount = commandArguments.Length;
+            return commandType != null;
+        }
+    }
+}
