786aadc1e1dfb278bdda442311010682b3284c4f
Rodrigo Dumont <rlsdumont@gmail.com>
2014-07-25 17:43:23 -0300

    Delete old projects

--- src/Cli/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Vtex.Toolbelt.Cli")]
-[assembly: ComVisible(false)]
-[assembly: Guid("7ed03da7-f475-4619-87ba-07e28cfa9a51")]
