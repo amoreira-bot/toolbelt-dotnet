98a1d6b9015519e56c73d9ead40f6d4899373d16
Alex Tercete <tercete@vtex.com.br>
2014-07-30 16:41:54 -0300

    Create a folder watcher for Apps

---
+++ src/Toolbelt/Services/AppWatcher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Vtex.Toolbelt.Model;
+
+namespace Vtex.Toolbelt.Services
+{
+    public class AppWatcher : Watcher
+    {
+        private readonly string _appName;
+        private readonly GalleryClient _galleryClient;
+
+        public AppWatcher(string appName, string rootPath, string authenticationToken, Configuration configuration)
+            : base(rootPath, configuration)
+        {
+            _appName = appName;
+            _galleryClient = new GalleryClient(rootPath, authenticationToken, configuration.GalleryEndpoint);
+        }
+
+        protected override void SendChanges(IList<Change> changes, bool resync)
+        {
+            _galleryClient.SendSandboxChanges(_appName, changes, resync);
+            base.SendChanges(changes, resync);
+        }
+    }
+}
