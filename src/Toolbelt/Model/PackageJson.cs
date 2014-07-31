e1020eae5e5de61dd873d895beaf1eda2f737f37
Anderson Moreira <anderson.moreira@vtex.com.br>
2014-07-31 17:32:42 -0300

    Validate version format

--- src/Toolbelt/Model/PackageJson.cs
+++ src/Toolbelt/Model/PackageJson.cs
@@ -2,18 +2,31 @@ using Newtonsoft.Json;
 using System;
 using System.IO;
 using Vtex.Toolbelt.Services;
+using System.Text.RegularExpressions;
 
 namespace Vtex.Toolbelt.Model
 {
     public class PackageJson
     {
         public string Name { get; set; }
+        public string Version { get; set; }
 
         public void Validate()
         {
             if (String.IsNullOrEmpty(this.Name))
             {
                 throw new ApplicationException("Field \"name\" should be set in package.json file");
+            }
+
+            if (String.IsNullOrEmpty(this.Version))
+            {
+                throw new ApplicationException("Field \"version\" should be set in package.json file");
+            }
+
+            var match = Regex.Match(this.Version, @"(\d+)\.(\d+)\.(\d+)");
+            if (!match.Success)
+            {
+                throw new ApplicationException("The version format is invalid");
             }
         }
 
